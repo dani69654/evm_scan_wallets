c754974655d43676a8282005e289f28be0cd2d81
dani69654 <60792849+dani69654@users.noreply.github.com>
2020-10-07 17:56:44 +0200

    Update Generate_Addresses_From_Seed.js

--- Generate_Addresses_From_Seed.js
+++ Generate_Addresses_From_Seed.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://mainnet.infura.io/v3/*Your infuta key*');
+const web3 = new Web3('https://mainnet.infura.io/v3/*Your infura key*');
 
 const bip39 = require('bip39');
 const hdkey = require('ethereumjs-wallet/hdkey');
